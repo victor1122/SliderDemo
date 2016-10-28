cf0a263ddc7abf846b795123d387142ca557f61b
AnhNHT <anhnht1996@gmail.com>
2016-10-28 08:56:20 +0700

    - Add dots at bottom page - Put resources name into arrays in xml files instead of hardcode - Optimize some function

--- app/src/main/java/com/example/anhnh/sliderdemo/MainActivity.java
+++ app/src/main/java/com/example/anhnh/sliderdemo/MainActivity.java
@@ -1,19 +1,77 @@
 package com.example.anhnh.sliderdemo;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     SwipeAdapter swipeAdapter;
+    private LinearLayout dotsLayout;
+    private Context context;
+    TextView[] dots;
+    int[] colorsInactive;
+    int[] colorsActive ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this.getApplicationContext();
+        //Show slider
         viewPager = (ViewPager) findViewById(R.id.view_paper);
         swipeAdapter = new SwipeAdapter(this);
         viewPager.setAdapter(swipeAdapter);
+        //set color array for dots and dots array size, must be put before call addBottomDots to prevent null
+        colorsInactive = viewPager.getResources().getIntArray(R.array.array_dot_inactive);
+        colorsActive = viewPager.getResources().getIntArray(R.array.array_dot_active);
+        dots = new TextView[colorsActive.length];
+        //create dots at bottom
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        addBottomDots(context);
+        //set action for dot state change
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                setActive(position, 5, dots);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+    private void addBottomDots(Context context) {
+        if (dotsLayout != null) {
+            dotsLayout.removeAllViews();
+        }
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(context);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dotsLayout.addView(dots[i]);
+        }
+    }
+
+    private void setActive(int position, int length, TextView[] dot){
+        for (int i = 0; i < dots.length; i++) {
+            dots[i].setTextColor(colorsInactive[position]);
+            if (dots.length > 0)
+                dots[position].setTextColor(colorsActive[position]);
+        }
     }
 }
--- app/src/main/java/com/example/anhnh/sliderdemo/SwipeAdapter.java
+++ app/src/main/java/com/example/anhnh/sliderdemo/SwipeAdapter.java
@@ -5,19 +5,22 @@ package com.example.anhnh.sliderdemo;
  */
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v4.view.PagerAdapter;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
 public class SwipeAdapter extends PagerAdapter {
-    private int[] imageSrc = {R.drawable.usa, R.drawable.vn, R.drawable.eng, R.drawable.jap, R.drawable.korea};
-    private String[] enName = {"U.S.A", "Vietnam", "England", "Japan", "Korea"};
-    private String[] japName = {"アメリカ", "ベトナム", "イギリス", "日本", "かんこく"};
+    private TypedArray image;
+    private String[] enName;
+    private String[] japName;
     private Context context;
     private LayoutInflater layoutInflater;
 
@@ -27,21 +30,26 @@ public class SwipeAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return imageSrc.length;
+        return enName.length;
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return view==(LinearLayout)object;
+        return view==(RelativeLayout)object;
     }
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View itemView = layoutInflater.inflate(R.layout.swipe_layout, container,false);
+        image = itemView.getResources().obtainTypedArray(R.array.country_flag);
+        enName = itemView.getResources().getStringArray(R.array.eng_name);
+        japName = itemView.getResources().getStringArray(R.array.jap_name);
+        //set layout
         ImageView imageView = (ImageView) itemView.findViewById(R.id.image_view);
         TextView textView = (TextView) itemView.findViewById(R.id.text_view);
-        imageView.setImageResource(imageSrc[position]);
+        //set data every slide
+        imageView.setImageResource(image.getResourceId(position,-1));
         textView.setText(enName[position]+"\n"+japName[position]);
         container.addView(itemView);
         return itemView;
@@ -49,6 +57,8 @@ public class SwipeAdapter extends PagerAdapter {
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        container.removeView((LinearLayout)object);
+        container.removeView((RelativeLayout)object);
     }
+
+
 }
